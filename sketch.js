0d837b45468e2cadf82e40045a23462b3df6cfd4
Kirill Trofimov <trofkm@gmail.com>
2023-02-03 22:56:36 +0300

    Add drawing align radius checkbox

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
 const flock = [];
+let showAlignRadius = false;
 function setup() {
-  createCanvas(900, 900);
- 
+  createCanvas(1800, 900);
+  setupLayout();
   for (let i = 0; i < 100; i++) {
     flock.push(new Boid());
   }
 }
-
+function setupLayout(){
+  let toggleAlignRadius = createCheckbox("Show Align Radius");
+  toggleAlignRadius.mousePressed(() => {
+    showAlignRadius = !showAlignRadius;
+  });
+}
 
 function draw() {
   background(51);
-  for(let boid of flock) {
+  for (let boid of flock) {
     boid.show();
     boid.update();
+    boid.flock(flock, showAlignRadius);
   }
 }
