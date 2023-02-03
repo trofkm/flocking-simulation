8f283a45c08721f3f4330219d734f5aff98ccee7
Kirill Trofimov <trofkm@gmail.com>
2023-02-03 22:56:01 +0300

    Update align behavior and add radius drawing

--- boid.js
+++ boid.js
@@ -2,17 +2,48 @@ class Boid {
     constructor() {
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(0.5, 1.5));
         this.acceleration = createVector();
+        this.alignRadius = random(10, 100);
+    }
+
+    align(boids, showRadius) {
+        let average = createVector();
+        let count = 0;
+        for (let other of boids) {
+            if (other != this && this.position.dist(other.position) < this.alignRadius) {
+                if (showRadius) {
+                    strokeWeight(3);
+                    stroke(100, 0, 0);
+                    noFill();
+                    circle(this.position.x, this.position.y, this.alignRadius);
+                }
+
+                average.add(other.velocity);
+                count++;
+            }
+
+
+        }
+        if (count > 0) {
+            average.div(count);
+            average.sub(this.velocity)
+        }
+        return average;
+    }
+    flock(boids, showAlignRadius) {
+        let alignment = this.align(boids, showAlignRadius);
+        this.acceleration = alignment;
     }
 
     update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
-        this.acceleration.mult(0.999);
-        if (this.position.x > width) this.position.x = 0;
-        if (this.position.x < 0) this.position.x = width;
-        if (this.position.y > height) this.position.y = 0;
-        if (this.position.y < 0) this.position.y = height;
+        // this.acceleration.mult(0.9);
+        if (this.position.x > width + 8) this.position.x = -7;
+        if (this.position.x < -8) this.position.x = width + 7;
+        if (this.position.y > height + 8) this.position.y = -7;
+        if (this.position.y < -8) this.position.y = height + 7;
     }
 
 
@@ -20,6 +51,5 @@ class Boid {
         strokeWeight(16);
         stroke(255);
         point(this.position.x, this.position.y);
-
     }
 }
